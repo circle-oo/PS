1cffa1032018d8f51d139e2bd782b97c35e6f476
circle-oo <woni8708@naver.com>
2021-02-08 09:54:06 +0900

    KOI03_H1

--- KOI/2003/KOI03_H1.cpp
+++ KOI/2003/KOI03_H1.cpp
@@ -2,34 +2,17 @@
 
 using namespace std;
 
-int n;
+int n, p;
 int child[10005][2]; // -1 : X
 int par[10005];
-int lr[10005]; //-1 : l, 1 : r, 0 : root
-int subt[10005]; // size of subtree
 pair<int, int> coor[10005];
 int rst[10005][2];
 
-int dfs(int x) {
-    if (x == -1) return 0;
-    return subt[x] = dfs(child[x][0]) + dfs(child[x][1]) + 1;
-}
-
-void solve(int x, int d) {
+void dfs(int x, int d) {
     if (x == -1) return;
-    if (lr[x] == 0) {
-        solve(child[x][0], d+1);
-        solve(child[x][1], d+1);
-        return;
-    }
-    if (lr[x] == -1) {
-        coor[x] = {d, coor[par[x]].second - subt[child[x][1] == -1 ? 0 : child[x][1]] - 1};
-    }
-    if (lr[x] == 1) {
-        coor[x] = {d, coor[par[x]].second + subt[child[x][0] == -1 ? 0 : child[x][0]] + 1};
-    }
-    solve(child[x][0], d+1);
-    solve(child[x][1], d+1);
+    dfs(child[x][0], d+1);
+    coor[x] = {d, ++p};
+    dfs(child[x][1], d+1);
 }
 
 int main() {
@@ -43,18 +26,12 @@ int main() {
         child[u][1] = r;
         par[l] = u;
         par[r] = u;
-        if (l != -1)
-            lr[l] = -1;
-        if (r != -1)
-            lr[r] = 1;
         rst[i][0] = INT_MAX;
         rst[i][1] = 0;
     }
     int root = 1;
     while (par[root] != 0) root = par[root];
-    dfs(root);
-    coor[root] = {root, subt[child[root][0] == -1 ? 0 : child[root][0]] + 1};
-    solve(root, 1);
+    dfs(root, 1);
     for (int i = 1; i <= n; i++) {
         rst[coor[i].first][0] = min(rst[coor[i].first][0], coor[i].second);
         rst[coor[i].first][1] = max(rst[coor[i].first][1], coor[i].second);
